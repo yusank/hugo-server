c677d7cafb7f1e0de07a02fc264b8f5349b3938b
yusank <yusankurban@gmail.com>
2020-03-09 20:18:28 +0800

    fix: auto update and restart web site by web hook

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,12 @@ echo "start deploy"
 echo "cross compile"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
 
+ssh $qcloud "sudo supervisorctl stop hugo-server"
+echo "stop server"
 scp $binFile $qcloud:$serverPath
+echo "file copied!"
+ssh $qcloud "sudo supervisorctl restart hugo-server"
+echo "server started!"
 
 rm -f $binFile
 echo "finish"
--- handler/router.go
+++ handler/router.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"bufio"
 	"encoding/json"
 	"github.com/yusank/klyn"
+	"io"
 	"log"
+	"net/http"
+	"os/exec"
 )
 
 // NewRouter - register router
@@ -12,6 +16,8 @@ func NewRouter(r *klyn.RouterGroup) {
 	{
 		githubRouter.POST("/webhook", githubWebhookHandler)
 	}
+
+	r.POST("/admin/web/restart",restartHandler)
 }
 
 func githubWebhookHandler(c *klyn.Context) {
@@ -21,6 +27,10 @@ func githubWebhookHandler(c *klyn.Context) {
 		return
 	}
 
+	if err := restartHugoWeb();err != nil {
+		log.Println("restart err:",err)
+	}
+
 	log.Println(f)
 	c.JSON(200, klyn.K{"errcode": 0})
 }
@@ -28,4 +38,43 @@ func githubWebhookHandler(c *klyn.Context) {
 func bindJson(v interface{}, c *klyn.Context) error {
 	d := json.NewDecoder(c.Request.Body)
 	return d.Decode(v)
+}
+
+func restartHandler(c *klyn.Context) {
+	if err := restartHugoWeb();err != nil {
+		c.JSON(http.StatusOK, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK,"ok")
+}
+
+func restartHugoWeb() error {
+	cmd :=  exec.Command("/bin/sh", "-c","/home/dev/hugo.yusank.space/deploy.sh")
+	log.Println(2,cmd.String())
+	out,err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	eOut,err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start();err != nil {
+		return err
+	}
+
+	go asyncScanner(out)
+	go asyncScanner(eOut)
+
+	return cmd.Wait()
+}
+
+func asyncScanner(r io.Reader) {
+	sc := bufio.NewScanner(r)
+	for sc.Scan() {
+		log.Println(sc.Text())
+	}
 }
